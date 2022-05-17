f54ea65e5b89d394620fa6b1ae223ec59e8f797e
Pasindu <pd67@students.waikato.ac.nz>
2022-05-17 12:44:23 +1200

    after npm has been built

--- assets/build/service-worker.js
+++ assets/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7daba7c11315f120b1dd2effd4531a2d.js"
+  "/precache-manifest.0477b4e907f4c947e4d7a00a71a70373.js"
 );
 
 self.addEventListener('message', (event) => {
